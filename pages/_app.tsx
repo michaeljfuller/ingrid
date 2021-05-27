2b54e5fca06270686231583539cba6d2297f95cb
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-05-27 16:40:05 +0100

    Add better Material-UI integration with NextJS
    Add server-side rendered styles and tidy up on the client.
    Add MaterialUiTheme wrapper.
    Change README

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import '../styles/globals.css'
 import type { AppProps as NextAppProps } from 'next/app'
+
+import '../styles/globals.css'
 import {WithLayout} from "../decorators/pages/UseLayout";
+import MaterialUiTheme from "../components/pages/MaterialUiTheme";
 
 export type AppProps = NextAppProps & {
   Component: WithLayout<NextAppProps['Component']>;
 };
 
+/**
+ * The root of the application.
+ * @link https://github.com/mui-org/material-ui/tree/master/examples/nextjs
+ */
 function MyApp({
   Component,
   pageProps
 }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page);
+  const page = getLayout( <Component {...pageProps} /> );
 
-  return getLayout(
-      <Component {...pageProps} />
-  );
+  // Remove the server-side injected CSS when client renders.
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) jssStyles.parentElement?.removeChild(jssStyles);
+  }, []);
+
+  return <>
+    <MaterialUiTheme>
+      {page}
+    </MaterialUiTheme>
+  </>;
 }
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Document, {Html, Head, Main, NextScript, DocumentContext} from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import theme from '../styles/material-ui/theme';
+
+/**
+ * @link https://nextjs.org/docs/advanced-features/custom-document
+ * @link https://material-ui.com/guides/server-rendering/
+ * @link https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
+ */
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+
+        // Render app and page and get the context of the page with collected side effects.
+        const sheets = new ServerStyleSheets();
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () => originalRenderPage({
+            enhanceApp: (
+                App
+            ) => {
+                return (props) => sheets.collect(<App {...props} />);
+            },
+        });
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            // Styles fragment is rendered after the app and page rendering finish.
+            styles: [
+                ...React.Children.toArray(initialProps.styles),
+                sheets.getStyleElement(),
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="theme-color" content={theme.palette.primary.main} /> {/* PWA primary color */}
+                    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
