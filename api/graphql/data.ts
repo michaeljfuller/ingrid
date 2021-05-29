478539ba24cacc5b47e4fd102805f25e9cd2fbba
Michael Fuller <33374714+michaeljfuller@users.noreply.github.com>
2021-05-29 18:34:51 +0100

    Make first items in infrastructure populated, rather than second

--- api/graphql/data.ts
+++ api/graphql/data.ts
@@ -1,13 +1,13 @@
 export const room1: Room = {id: 'r101', name: 'Room 101'} ;
 export const room2: Room = {id: 'r102', name: 'Room 102'} ;
 
-export const floor1: Floor = { id: 'f1', name: '1st Floor', rooms: [] };
-export const floor2: Floor = { id: 'f2', name: '2nd Floor', rooms: [room1, room2] };
+export const floor1: Floor = { id: 'f1', name: '1st Floor', rooms: [room1, room2] };
+export const floor2: Floor = { id: 'f2', name: 'Empty Floor', rooms: [] };
 
-export const building1: Building = { id: 'b1', name: 'First Building', floors: [] };
-export const building2: Building = { id: 'b2', name: 'Second Building', floors: [floor1, floor2] };
+export const building1: Building = { id: 'b1', name: 'First Building', floors: [floor1, floor2] };
+export const building2: Building = { id: 'b2', name: 'Empty Building', floors: [] };
 
-export const region1: Region = { id: 'r1', name: 'Region 1', buildings: [] }
-export const region2: Region = { id: 'r2', name: 'Region 2', buildings: [building1, building2] }
+export const region1: Region = { id: 'r1', name: 'Region 1', buildings: [building1, building2] }
+export const region2: Region = { id: 'r2', name: 'Empty Region', buildings: [] }
 
 export const infrastructure: Infrastructure = { regions: [region1, region2] };
